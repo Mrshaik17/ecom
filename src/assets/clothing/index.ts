a16556c74a5bce82e1015717ad7cf324c83aadc4
Shaik Touheed <shaikthouheed17@gmail.com>
2025-10-11 18:11:47 +0530

    Update index.ts

--- src/assets/clothing/index.ts
+++ src/assets/clothing/index.ts
@@ -7,7 +7,40 @@ import clothing6 from './5.jpg';
 import clothing7 from './6.jpg';
 import clothing8 from './7.jpg';
 import clothing9 from './8.jpg';
-
+import clothing10 from './9.jpg';
+import clothing11 from './10.jpg';
+import clothing12 from './11.jpg';
+import clothing13 from './12.jpg';
+import clothing14 from './13.jpg';
+import clothing15 from './14.jpg';
+import clothing16 from './15.jpg';
+import clothing18 from './17.jpg';
+import clothing19 from './18.jpg';
+import clothing20 from './19.jpg';
+import clothing21 from './20.jpg';
+import clothing22 from './24.jpg';
+import clothing23 from './25.jpg';
+import clothing24 from './26.jpg';
+import clothing25 from './29.jpg';
+import clothing26 from './30.jpg';
+import clothing27 from './31.jpg';
+import clothing28 from './32.jpg';
+import clothing29 from './33.jpg';
+import clothing30 from './34.jpg';
+import clothing31 from './43.jpg';
+import clothing32 from './44.jpg';
+import clothing33 from './45.jpg';
+import clothing34 from './46.jpg';
+import clothing35 from './47.jpg';
+import clothing36 from './48.jpg';
+import clothing37 from './49.jpg';
+import clothing38 from './50.jpg';
+import clothing39 from './51.jpg';
+import clothing40 from './52.jpg';
+import clothing41 from './53.jpg';
+import clothing42 from './54.jpg';
+import clothing43 from './55.jpg';
+import clothing44 from './56.jpg';
 
 
 export const clothingImages = [
@@ -19,14 +52,90 @@ export const clothingImages = [
   clothing6,
   clothing7,
   clothing8,
-  clothing9
-  
-   // ✅ added Car.jpg into array
-  // Add more clothing images here as you import them
-  // clothing3,
-  // clothing4,
-  // etc...
+  clothing9,
+  clothing10,
+  clothing11,
+  clothing12,
+  clothing13,
+  clothing14,
+  clothing15,
+  clothing16,
+  clothing18,
+  clothing19,
+  clothing20,
+  clothing21,
+  clothing22,
+  clothing23,
+  clothing24,
+  clothing25,
+  clothing26,
+  clothing27,
+  clothing28,
+  clothing29, 
+  clothing30,
+  clothing31,
+  clothing32,
+  clothing33,
+  clothing34,
+  clothing35,
+  clothing36,
+  clothing37,
+  clothing38,
+  clothing39,
+  clothing40,
+  clothing41,
+  clothing42,
+  clothing43,
+  clothing44
+
+
+
 ];
 
 // Export individual images if needed  
-export { clothing1,clothing2, clothing3, clothing4, clothing5, clothing6, clothing7, clothing8, clothing9 };
+export {
+  clothing1,
+  clothing2,
+  clothing3,
+  clothing4,
+  clothing5,
+  clothing6,
+  clothing7,
+  clothing8,
+  clothing9,
+  clothing10,
+  clothing11,
+  clothing12,
+  clothing13,
+  clothing14,
+  clothing15,
+  clothing16,
+  clothing18,
+  clothing19,
+  clothing20,
+  clothing21,
+  clothing22,
+  clothing23,
+  clothing24,
+  clothing25,
+  clothing26,
+  clothing27,
+  clothing28,
+  clothing29,
+  clothing30,
+  clothing31,
+  clothing32,
+  clothing33,
+  clothing34,
+  clothing35,
+  clothing36,
+  clothing37,
+  clothing38,
+  clothing39,
+  clothing40,
+  clothing41,
+  clothing42,
+  clothing43,
+  clothing44
+
+};
