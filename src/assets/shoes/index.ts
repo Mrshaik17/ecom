3c6e2cefe254d919118e1b66988e7871f2bbbd14
Shaik Touheed <shaikthouheed17@gmail.com>
2025-10-11 18:10:58 +0530

    Update index.ts
    Shoes index updated succesfuuly upto now

--- src/assets/shoes/index.ts
+++ src/assets/shoes/index.ts
@@ -5,6 +5,48 @@ import shoe4 from './4.jpg';
 import shoe5 from './5.jpg';
 import shoe6 from './10.jpg';
 import shoe7 from './11.jpg';
+import shoe8 from './12.jpg';
+import shoe9 from './13.jpg';
+import shoe10 from './14.jpg';
+import shoe11 from './15.jpg';
+import shoe12 from './16.jpg';
+import shoe13 from './17.jpg';
+import shoe14 from './18.jpg';
+import shoe15 from './19.jpg';
+import shoe16 from './20.jpg';
+import shoe17 from './21.jpg';
+import shoe18 from './22.jpg';
+import shoe19 from './23.jpg';
+import shoe20 from './24.jpg';
+import shoe21 from './25.jpg';
+import shoe22 from './26.jpg';
+import shoe23 from './27.jpg';
+import shoe24 from './28.jpg';
+import shoe25 from './29.jpg';
+import shoe26 from './30.jpg';
+import shoe27 from './31.jpg';
+import shoe28 from './32.jpg';
+import shoe29 from './33.jpg';
+import shoe30 from './34.jpg';
+import shoe31 from './38.jpg';
+import shoe32 from './39.jpg';
+import shoe33 from './40.jpg';
+import shoe34 from './41.jpg';
+import shoe35 from './44.jpg';
+import shoe36 from './45.jpg';
+import shoe37 from './46.jpg';
+import shoe38 from './49.jpg';
+import shoe39 from './50.jpg';
+import shoe40 from './51.jpg';
+import shoe41 from './52.jpg';
+import shoe42 from './53.jpg';
+import shoe43 from './61.jpg';
+import shoe44 from './62.jpg';
+import shoe45 from './63.jpg';
+import shoe46 from './64.jpg';
+import shoe47 from './65.jpg';
+
+
 
 
 
@@ -15,7 +57,49 @@ export const shoeImages = [
   shoe4,
   shoe5,
   shoe6, 
-  shoe7
+  shoe7,
+  shoe8,
+  shoe9, 
+  shoe10,
+  shoe11,
+  shoe12,
+  shoe13,
+  shoe14,
+  shoe15,
+  shoe16,
+  shoe17,
+  shoe18,
+  shoe19,
+  shoe20,
+  shoe21,
+  shoe22,
+  shoe23,
+  shoe24,
+  shoe25,
+  shoe26,
+  shoe27,
+  shoe28,
+  shoe29,
+  shoe30,
+  shoe31,
+  shoe32,
+  shoe33,
+  shoe34,
+  shoe35,
+  shoe36,
+  shoe37,
+  shoe38,
+  shoe39,
+  shoe40, 
+  shoe41,
+  shoe42,
+  shoe43,
+  shoe44,
+  shoe45,
+  shoe46,
+  shoe47
+  
+
 
   // Add more shoe images here as you import them
   // shoe3,
@@ -24,4 +108,7 @@ export const shoeImages = [
 ];
 
 // Export individual images if needed
-export { shoe1, shoe2, shoe3, shoe4, shoe5, shoe6, shoe7 };
+export { shoe1, shoe2, shoe3, shoe4, shoe5, shoe6, shoe7, shoe8, shoe9, shoe10, shoe11, shoe12 };
+export { shoe13, shoe14, shoe15, shoe16, shoe17, shoe18, shoe19, shoe20, shoe21, shoe22, shoe23, shoe24, shoe25, shoe26, shoe27, shoe28, shoe29, shoe30}; 
+export { shoe31, shoe32, shoe33, shoe34, shoe35, shoe36, shoe37, shoe38, shoe39, shoe40, shoe41, shoe42};
+export { shoe43, shoe44, shoe45, shoe46, shoe47};
